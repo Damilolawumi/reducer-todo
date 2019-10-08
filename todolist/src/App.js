6ec7a89eaf5ba7b336b44489dc26944c8d4b039b
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-08 20:20:02 +0100

    imported Todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Todolist from '../src/Todolist';
-
+import React, {useReducer} from 'react';
 import './App.css';
+import Todoform from './Todoform';
 import Todolist from './Todolist';
+import { initialState, todoReducer } from './reducers/todosReducer'
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
   return (
     <div className="App">
-      <Todolist/>
+      <Todolist state={state} dispatch={dispatch}/>
+      <Todoform dispatch={dispatch}/>
     </div>
   );
 }
