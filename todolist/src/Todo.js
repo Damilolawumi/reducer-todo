d6d2bd47844cce9eb13d575336d5afa6bfc57241
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-08 20:21:09 +0100

    in the Todo.js created a function toggletodo and added an onclik event and a conditional statement

--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Todo (props) {
+    const toggleTodo = () => {
+        props.dispatch({
+            type:"TOGGLE_TODO",
+            todo: props.todo
+        })
+        
+        // props.toggleTodo(props.todo.id)
+    }
+    const todoStyle = {};
+
+    if(props.todo.completed) {
+        todoStyle.textDecoration = "line-through"
+    }
 
     return (
-        <div>
+        <div onClick={toggleTodo} style={todoStyle}>
             {props.todo.item}
             {props.todo.completed}
             {props.todo.id}
